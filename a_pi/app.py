b7dca1cdaf5bb22310b88210c57f917cf9159701
Quentin Boyer <qbsecond@gmail.com>
2019-02-01 09:29:09 +0100

    post

--- a_pi/app.py
+++ a_pi/app.py
@@ -1,36 +1,9 @@
 from flask import Flask
 import sqlite3
 import click
-from flask import current_app, g
+from flask import current_app, g, request
 from flask.cli import with_appcontext
 import os
-
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
-    return app
 
 def get_db():
     if 'db' not in g:
@@ -41,7 +14,6 @@ def get_db():
         g.db.row_factory = sqlite3.Row
 
     return g.db
-
 
 def close_db(e=None):
     db = g.pop('db', None)
@@ -87,9 +59,11 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/')
+    @app.route('/', methods=('GET', 'POST'))
     def hello():
-        return 'Hello, World!'
+        #if request.method == 'POST':
+        digit = request.form['digit']
+        return 'Hello, World! : {}'.format(digit)
     
     init_app(app)
 
