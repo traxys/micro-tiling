5ef8407db249f939d2da54e18f02a5bf9f2d4dde
Quentin Boyer <qbsecond@gmail.com>
2019-02-21 00:36:38 +0100

    Functiooooooons

--- init/init.py
+++ init/init.py
@@ -1,16 +1,17 @@
 import random
 import requests
-
-from matrix_client.client import MatrixClient
-client = MatrixClient("http://localhost:8008")
-
 import json
-matrix_param_file = open("../matrix_user.json","r")
-matrix_param = json.loads(matrix_param_file.read())
-matrix_param_file.close()
+from matrix_client.client import MatrixClient
 
-token = client.login(username=matrix_param["username"], password=matrix_param["password"])
-room = client.join_room(matrix_param["room"]);
+def get_room():
+    client = MatrixClient("http://localhost:8008")
+
+    matrix_param_file = open("../matrix_user.json","r")
+    matrix_param = json.loads(matrix_param_file.read())
+    matrix_param_file.close()
+
+    token = client.login(username=matrix_param["username"], password=matrix_param["password"])
+    return client.join_room(matrix_param["room"]);
 
 def make_pi(total):
     q, r, t, k, m, x = 1, 0, 1, 1, 3, 3
@@ -29,26 +30,31 @@ A_PI_ADDR = 'http://127.0.0.1:5000'
 MAX_SEGMENT = 20
 MIN_SEGMENT = 5
 
-init = {'digit': '3', 'job': ''}
-r = requests.post(A_PI_ADDR, data=init)
-job_id = r.text
-room.send_text('@' + json.dumps({
-    "msg": "Job started",
-    "service": "init",
-    "id": job_id
-}))
+def generate_segments():
+    init = {'digit': '3', 'job': ''}
+    r = requests.post(A_PI_ADDR, data=init)
+    job_id = r.text
+    
+    room = get_room()
+    room.send_text('@' + json.dumps({
+        "msg": "Job started",
+        "service": "init",
+        "id": job_id
+    }))
 
-pi = make_pi(random.randint(MIN_SEGMENT, MAX_SEGMENT))
-next(pi)
+    pi = make_pi(random.randint(MIN_SEGMENT, MAX_SEGMENT))
+    next(pi)
 
-for digit in pi:
-    payload = {'digit': str(digit), 'job': job_id}
-    r = requests.post(A_PI_ADDR, data=payload)
+    for digit in pi:
+        payload = {'digit': str(digit), 'job': job_id}
+        r = requests.post(A_PI_ADDR, data=payload)
 
-room.send_text('@' + json.dumps({
-    "msg": "Finished generating segments",
-    "service": "init",
-    "id": job_id
-}))
-done = {'digit': 'π', 'job': job_id}
-r = requests.post(A_PI_ADDR, data=done)
+    room.send_text('@' + json.dumps({
+        "msg": "Finished generating segments",
+        "service": "init",
+        "id": job_id
+    }))
+    done = {'digit': 'π', 'job': job_id}
+    r = requests.post(A_PI_ADDR, data=done)
+
+generate_segments()
