ebe0caddc620ad1cdf845058e94388363e705056
AurelienMoisson <>
2019-03-03 18:06:59 +0100

    new clipping code in unitator

---
+++ Unitator/clipping.py
@@ -0,0 +1,57 @@
+#! /usr/bin/python3
+
+class p:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __add__(self, other):
+        return p(self.x+other.x, self.y+other.y)
+    def __sub__(self, other):
+        return p(self.x-other.x, self.y-other.y)
+    def __mul__(self, other):
+        return p(self.x*other, self.y*other)
+    def __str__(self):
+        return '('+str(self.x)+', '+str(self.y)+')'
+
+class segment:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def __str__(self):
+        return '('+str(self.a)+', '+str(self.b)+')'
+    def __repr__(self):
+        return str(self)
+
+
+lines = [segment(p(0,0.4), p(1.5, 1.5))]
+
+def clip_right(lines):
+    out = []
+    for l in lines:
+        if l.a.x == l.b.x:
+            if l.a.x<1:
+                out.append(l)
+        else:
+            k = (l.a.x-1)/(l.a.x-l.b.x)
+            ab = p(l.b.x-l.a.x, l.b.y-l.a.y)
+            if 0<k<1:
+                if l.a.x > l.b.x:
+                    out.append(segment(l.a + ab*k, l.b))
+                else:
+                    out.append(segment(l.a, l.b - ab*(1-k)))
+            else:
+                out.append(l)
+    return out
+
+def turn_right(lines):
+    for l in lines:
+        l.a.x, l.a.y = l.a.y, -l.a.x
+        l.b.x, l.b.y = l.b.y, -l.b.x
+
+
+def clip_unit_square(lines):
+    for _ in range(4):
+        turn_right(lines)
+        lines = clip_right(lines)
+    return lines
+
