d6f0445e6a6996de289d32e8669bcce49f5b803b
Quentin Boyer <qbsecond@gmail.com>
2019-03-06 23:38:40 +0100

    Cleanup

--- start_services.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-SERVICE_ROOT=$(dirname $BASH_SOURCE)
-SERVICE_ROOT=$(realpath $SERVICE_ROOT)
-cd $SERVICE_ROOT/gopher; python golfer.py & cd $SERVICE_ROOT/a_pi; flask run & cd $SERVICE_ROOT/Millllllll; python millllllll.py
