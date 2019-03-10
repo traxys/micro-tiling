248254631be25c6b9cad96fc3d416b07e3a13165
Quentin Boyer <qbsecond@gmail.com>
2019-03-10 16:11:23 +0100

    General doc

---
+++ generate_doc.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+
+make -C Millllllll/gopher html
+mkdir -p doc/golfer
+cp -r Millllllll/gopher/build/html/* doc/golfer
+
+
+for service in API a_pi Millllllll client_library Unitator
+do
+	make -C $service html
+	mkdir -p doc/$service
+	cp -r $service/build/html/* doc/$service
+done
